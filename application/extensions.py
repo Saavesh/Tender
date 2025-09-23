a21c27eebca8d901d128109ee7350bd5186f3198
Sherry Saavedra <shsa7018@colorado.edu>
2025-09-23 00:11:53 -0700

    add optional path for flask_mailman

--- application/extensions.py
+++ application/extensions.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
 from flask_migrate import Migrate
 from flask_security import Security
-from flask_mailman import Mail
+
+try:
+    from flask_mailman import Mail
+except ImportError:
+    Mail = None
 
 db = SQLAlchemy()
 cache = Cache()
 migrate = Migrate()
 security = Security()
-mail = Mail()
+mail = Mail() if Mail else None
